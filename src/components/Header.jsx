7526b9b1f2a79500b54ddfc183042717e3e437c1
nporia <Porianikita262@gmail.com>
2022-02-27 10:18:25 +0530

    Add files via upload

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Logo from './logo.png'
+function Header() {
+  return (
+    <header>
+      <h1>
+      <img src = {Logo} />
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
